a3d0a9383900e95337a1fac10c1f7260cd02b470
shohan-pherones <shohan.sub.56@gmail.com>
2024-03-18 18:18:57 +0600

    validate auth audience

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middlewares/auth";
 
 const router = express.Router();
 
 // /api/my/user
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 
 export default router;
