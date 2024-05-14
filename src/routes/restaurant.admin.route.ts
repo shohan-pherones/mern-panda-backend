fddabf53c180c65a664ad17dbf4678f7e35c7dc5
shohan-pherones <shohan.sub.56@gmail.com>
2024-05-14 20:33:39 +0600

    get public restaurant by id

--- src/routes/restaurant.admin.route.ts
+++ src/routes/restaurant.admin.route.ts
@@ -1,8 +1,21 @@
 import express, { Router } from "express";
 import { param } from "express-validator";
-import { searchRestaurant } from "../controllers/restaurant.admin.controller";
+import {
+  getRestaurant,
+  searchRestaurant,
+} from "../controllers/restaurant.admin.controller";
 
 const router: Router = express.Router();
+
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("Restaurant ID parameter must be a valid string"),
+  getRestaurant
+);
 
 router.get(
   "/search/:city",
